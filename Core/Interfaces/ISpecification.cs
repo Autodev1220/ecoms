1e952390470ca01d81396fffc4acfc6fed7aecf5
Allanv <allanjay.valdez1220@gmail.com>
2025-05-28 06:08:43 +0800

    Refactoring BaseSpecification

--- Core/Interfaces/ISpecification.cs
+++ Core/Interfaces/ISpecification.cs
@@ -3,13 +3,17 @@ using System.Linq.Expressions;
 
 namespace Core.Interfaces;
 
+//spec
 public interface ISpecification<T>
 {
     Expression<Func<T, bool>>? Criteria { get; }
     Expression<Func<T, object>>? OrderBy { get; }
     Expression<Func<T, object>>? OrderByDescending { get; }
+    
+    bool IsDistinct { get; }
 }
 
+//for projection
 public interface ISpecification<T, TResult> : ISpecification<T>
 {
     Expression<Func<T, TResult>>? Select { get; }
--- Core/Specification/BaseSpecification.cs
+++ Core/Specification/BaseSpecification.cs
@@ -4,14 +4,26 @@ using Core.Interfaces;
 
 namespace Core.Specification;
 
-public class BaseSpecification<T>(Expression<Func<T, bool>>? criteria) : ISpecification<T>
+//specification
+public class BaseSpecification<T> : ISpecification<T>
 {
+    private readonly Expression<Func<T, bool>>? _criteria;
+
+    protected BaseSpecification(Expression<Func<T, bool>>? criteria)
+    {
+        _criteria = criteria;
+    }
+
+    //default constructor that sets no filter
     protected BaseSpecification() : this(null) { }
-    public Expression<Func<T, bool>>? Criteria => criteria;
+
+    public Expression<Func<T, bool>>? Criteria => _criteria;
 
     public Expression<Func<T, object>>? OrderBy { get; private set; }
 
     public Expression<Func<T, object>>? OrderByDescending { get; private set; }
+
+    public bool IsDistinct { get; private set; }
 
     protected void AddOrderBy(Expression<Func<T, object>> orderByExpression)
     {
@@ -23,5 +35,26 @@ public class BaseSpecification<T>(Expression<Func<T, bool>>? criteria) : ISpecif
         OrderByDescending = orderByDescExpression;
     }
 
+    protected void ApplyDistinct()
+    {
+        IsDistinct = true;
+    }
 
+
+}
+
+//projection
+public class BaseSpecification<T, TResult> : BaseSpecification<T>, ISpecification<T, TResult>
+{
+    protected BaseSpecification(Expression<Func<T, bool>>? criteria) : base(criteria)
+    {
+    }
+    protected BaseSpecification() : this(null) { }
+    
+    public Expression<Func<T, TResult>>? Select { get; private set; }
+
+    protected void AddSelect(Expression<Func<T, TResult>> selectExpression)
+    {
+        Select = selectExpression;
+    }
 }
